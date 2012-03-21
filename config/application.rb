4ed376e171178e8ba54ebfde9727dd2077ff535c
unknown <dwayne@.(none)>
2012-03-21 18:32:53 -0500

    launch test

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,9 @@ module Filetunnel
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+config.assets.precompile << '*.js'
+    
+
   end
 end
