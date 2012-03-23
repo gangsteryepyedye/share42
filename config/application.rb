896afda1c925dc02c1ccd8f11031ada4f8ba648e
unknown <dwayne@.(none)>
2012-03-23 10:16:59 -0500

    changed url

--- config/application.rb
+++ config/application.rb
@@ -46,8 +46,8 @@ module Filetunnel
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-config.assets.precompile << '*.js'
-    
+    config.assets.precompile << '*.js'
+
 
   end
 end
