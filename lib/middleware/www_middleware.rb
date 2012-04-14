e94ee175c983b46382e778f195532ab753fe4c70
unknown <dwayne@dwayne-dwayne.(none)>
2012-04-14 15:03:51 -0500

    er

--- lib/middleware/www_middleware.rb
+++ lib/middleware/www_middleware.rb
@@ -1,17 +1,19 @@
+
+
 class WwwMiddleware
-  def initialize(app)
-    @app = app
-  end
+def initialize(app)
+@app = app
+end
 
-  def call(env)
-    request = Rack::Request.new(env)
-    if request.host.starts_with?("www.")
-      [301, {"Location" => request.url.sub("//www.", "//")}, self]
-    else
-      @app.call(env)
-    end
-  end
+def call(env)
+request = Rack::Request.new(env)
+if request.host.starts_with?(“*.com”)
+[301, {"Location" => request.url.sub("//*.com", "//www.*.com")}, self]
+else
+@app.call(env)
+end
+end
 
-  def each(&block)
-  end
+def each(&block)
+end
 end
